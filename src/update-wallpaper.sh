5082dbdc6082fb5956db8e9d9de6613d3a817d6d
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2026-02-25 15:52:01 +0100

    allow calling from anywhere

--- src/update-wallpaper.sh
+++ src/update-wallpaper.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 wallpapers=~/Pictures/Wallpapers
 
 screens="$(xrandr --query | grep -P '^\s*\w+\s+connected.*\+(\d+)\+(\d+)')"
